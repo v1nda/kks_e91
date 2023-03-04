61b0fa01977e804a8c193fad726f55d6c4ae84c4
v1nda <v1nda@local>
2023-03-04 21:34:58 +0300

    Версия 4. Реализована Ева. Функции передачи вынесны в func.py. Необходимо дальнейшее совершенствование реализации Евы.

--- e91.py
+++ e91.py
@@ -2,95 +2,6 @@ import func
 import sys, argparse
 
 
-def do_one_iteration(bA, bB, eva=False, circ=True):
-
-        qA = func.cirq.NamedQubit("Alice")
-        qB = func.cirq.NamedQubit("Bob")
-
-        circuit = func.cirq.Circuit()
-        circuit.append(func.singlet(qA, qB))
-
-        bases_coincided = func.basis_comparison(bA, bB)
-        if not bases_coincided:
-                print("Different bases are chosen:")
-                print("Value is discarded.")
-
-                return
-
-        circuit.append(func.alice_basis_construction(bA, qA))
-        circuit.append(func.bob_basis_construction(bB, qB))
-
-        circuit.append(func.cirq.X(qB))
-        circuit.append(func.cirq.measure(qA, qB))
-
-        if circ:
-                print(circuit)
-        simulator = func.cirq.Simulator()
-        result = simulator.run(circuit, repetitions=1)
-
-        print(result)
-
-def key_generation(length, stat=False):
-
-        qA = func.cirq.NamedQubit("Alice")
-        qB = func.cirq.NamedQubit("Bob")
-
-        basesA = [func.random() for i in range(length)]
-        basesB = [func.random() for i in range(length)]
-
-        keyA = []
-        keyB = []
-        clean_keyA = []
-        clean_keyB = []
-        discarded_keyA = []
-        discarded_keyB = []
-        
-        for b in range(length):
-
-                circuit = func.cirq.Circuit()
-                circuit.append(func.singlet(qA, qB))
-
-                circuit.append(func.alice_basis_construction(basesA[b], qA))
-                circuit.append(func.bob_basis_construction(basesB[b], qB))
-
-                circuit.append(func.cirq.X(qB))
-                circuit.append(func.cirq.measure(qA, qB, key='result'))
-
-                simulator = func.cirq.Simulator()
-                result = simulator.run(circuit, repetitions=1)
-
-                keyA.append(int(result.measurements['result'][0][0]))
-                keyB.append(int(result.measurements['result'][0][1]))
-
-
-        # Установление индексов совпавших базисов и формирование Алисой и Бобом своих чистых и отброшенных ключей
-        for b in range(length):
-                
-                if func.basis_comparison(basesA[b], basesB[b]):
-                        clean_keyA.append(keyA[b])
-                        clean_keyB.append(keyB[b])
-                else:
-                        discarded_keyA.append(keyA[b])
-                        discarded_keyB.append(keyB[b])
-        
-        # Вывод чистых ключей
-        clean_keyA_str = ""
-        clean_keyB_str = ""
-        for b in range(len(clean_keyA)):
-                clean_keyA_str += str(clean_keyA[b])
-                clean_keyB_str += str(clean_keyB[b])
-
-        print("Alice key (" + str(len(clean_keyA)) + " bits):")
-        print(clean_keyA_str)
-        print("Bob key (" + str(len(clean_keyB)) + " bits):")
-        print(clean_keyB_str)
-
-        if stat:
-                func.key_statistic_calc(keyA, keyB, clean_keyA)
-
-        return
-
-
 def create_parser ():
 
         parser = argparse.ArgumentParser(
@@ -105,6 +16,7 @@ def create_parser ():
         gen_parser = subparsers.add_parser('generate_key', help='режим генерации ключа')
         gen_parser.add_argument('-l', '--length', default=1000, type=int, help='количество передаваемых бит')
         gen_parser.add_argument('-s', '--stat', action='store_true', help='вывод статистики по ключу')
+        gen_parser.add_argument('-e', '--eva', action='store_true', help='передача с участием злоумышленника')
         gen_parser.set_defaults(func=parse_generate_key)
 
         bit_parser = subparsers.add_parser('one_bit', help='передача одного бита')
@@ -117,16 +29,16 @@ def create_parser ():
 
 def parse_generate_key(args):
 
-        key_generation(args.length, stat=args.stat)
+        func.key_generation(args.length, stat=args.stat, eva=args.eva)
 
 def parse_one_bit(args):
 
         if args.basis == 'pi/4':
-                do_one_iteration(1, 0, eva=args.eva, circ=args.circuit)
+                func.do_one_iteration(1, 0, eva=args.eva, circ=args.circuit)
         elif args.basis == 'pi/2':
-                do_one_iteration(2, 1, eva=args.eva, circ=args.circuit)
+                func.do_one_iteration(2, 1, eva=args.eva, circ=args.circuit)
         else:
-                do_one_iteration(func.random(), func.random(), eva=args.eva, circ=args.circuit)
+                func.do_one_iteration(func.random(), func.random(), eva=args.eva, circ=args.circuit)
 
 
 def main():
--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 import cirq
 import math
 
-def random():
+def random(mod=3):
 
         q0 = cirq.LineQubit.range(1)[0]
         circuit = cirq.Circuit(
@@ -9,9 +9,9 @@ def random():
                 cirq.MeasurementGate(1).on(q0),
         )
 
-        res = 3
+        res = mod
 
-        while res == 3:
+        while res >= mod:
 
                 simulator = cirq.Simulator()
                 result = simulator.run(circuit, repetitions=2)
@@ -67,6 +67,19 @@ def bob_basis_construction(basis, q):
         
         return moments
 
+def eve_basis_construction(q):
+
+        moments = []
+        basis = random(mod=2)
+
+        if basis == 0:
+                moments.append(cirq.rz(math.pi/2).on(q))
+                moments.append(cirq.H(q))
+                moments.append(cirq.rz(math.pi/4).on(q))
+                moments.append(cirq.H(q))
+
+        return moments
+
 def basis_comparison(b0, b1):
 
         if (b0 == 1 and b1 == 0) or (b0 == 2 and b1 == 1):
@@ -107,5 +120,129 @@ def key_statistic_calc(key0, key1, clean_key):
         print('Статистика по чистому ключу:')
         print('\tдлина ключа: \t\t\t' + str(len(clean_key)))
         print('\tвероятность появления 0: \t' + str(p0_clean_key) + '%')
+
+        return
+
+def eva_key_statistic(key, keyE):
+
+        pT = 0
+        for i in range(len(key)):
+                
+                if key[i] == keyE[i]:
+                        pT += 1
+                
+        pT = round(pT / len(key) * 100, 4)
+
+        print()
+        print('Статистика угадывания Евой: \t', pT)
+
+
+
+def do_one_iteration(bA, bB, eva=False, circ=True):
+
+        qA = cirq.NamedQubit("Alice")
+        qB = cirq.NamedQubit("Bob")
+
+        circuit = cirq.Circuit()
+        circuit.append(singlet(qA, qB))
+
+        bases_coincided = basis_comparison(bA, bB)
+        if not bases_coincided:
+                print("Алисой и Бобом выбраны различные базисы:")
+                print("\tзначение отброшено.")
+
+                return
+
+        circuit.append(alice_basis_construction(bA, qA))
+        circuit.append(bob_basis_construction(bB, qB))
+
+        if eva:
+                circuit.append(eve_basis_construction(qB))
+                circuit.append(cirq.measure(qB, key='Eva'))
+
+        circuit.append(cirq.X(qB))
+        circuit.append(cirq.measure(qA, qB, key='result'))
+
+        if circ:
+                print(circuit)
+        simulator = cirq.Simulator()
+        result = simulator.run(circuit, repetitions=1)
+
+        print('Результат передачи:')
+        print('\tАлиса: \t', int(result.measurements['result'][0][0]))
+        print('\tБоб: \t', int(result.measurements['result'][0][1]))
+        if eva:
+                print('Результат перехвата:')
+                print('\tЕва: \t', (int(result.measurements['Eva'][0][0]) + 1) % 2)
+
+def key_generation(length, eva=False, stat=False):
+
+        qA = cirq.NamedQubit("Alice")
+        qB = cirq.NamedQubit("Bob")
+
+        basesA = [random() for i in range(length)]
+        basesB = [random() for i in range(length)]
+
+        keyA = []
+        keyB = []
+        keyE = []
+        clean_keyA = []
+        clean_keyB = []
+        clean_keyE = []
+        discarded_keyA = []
+        discarded_keyB = []
+        
+        for b in range(length):
+
+                circuit = cirq.Circuit()
+                circuit.append(singlet(qA, qB))
+
+                circuit.append(alice_basis_construction(basesA[b], qA))
+                circuit.append(bob_basis_construction(basesB[b], qB))
+
+                if eva:
+                        circuit.append(eve_basis_construction(qB))
+                        circuit.append(cirq.measure(qB, key='Eva'))
+
+                circuit.append(cirq.X(qB))
+                circuit.append(cirq.measure(qA, qB, key='result'))
+
+                simulator = cirq.Simulator()
+                result = simulator.run(circuit, repetitions=1)
+
+                keyA.append(int(result.measurements['result'][0][0]))
+                keyB.append(int(result.measurements['result'][0][1]))
+
+                if eva:
+                        keyE.append((int(result.measurements['Eva'][0][0]) + 1) % 2)
+
+
+        # Установление индексов совпавших базисов и формирование Алисой и Бобом своих чистых и отброшенных ключей
+        for b in range(length):
+                
+                if basis_comparison(basesA[b], basesB[b]):
+                        clean_keyA.append(keyA[b])
+                        clean_keyB.append(keyB[b])
+                        clean_keyE.append(keyE[b])
+                else:
+                        discarded_keyA.append(keyA[b])
+                        discarded_keyB.append(keyB[b])
+        
+        # Вывод чистых ключей
+        clean_keyA_str = ""
+        clean_keyB_str = ""
+        for b in range(len(clean_keyA)):
+                clean_keyA_str += str(clean_keyA[b])
+                clean_keyB_str += str(clean_keyB[b])
+
+        print("Alice key (" + str(len(clean_keyA)) + " bits):")
+        print('<' + clean_keyA_str + '>')
+        print("Bob key (" + str(len(clean_keyB)) + " bits):")
+        print('<' + clean_keyB_str + '>')
+
+        if stat:
+                key_statistic_calc(keyA, keyB, clean_keyA)
+                if eva:
+                        eva_key_statistic(clean_keyA, clean_keyE)
 
         return
